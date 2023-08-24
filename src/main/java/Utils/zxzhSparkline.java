73e75c222e3cd260fc5b562d4ce90fb9982c7437
zhouhengtao <java_ht@163.com>
2023-08-24 15:36:49 +0800

    fix

--- src/main/java/Utils/zxzhSparkline.java
+++ src/main/java/Utils/zxzhSparkline.java
@@ -15,7 +15,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.*;
 
-
 public class zxzhSparkline extends AbstractJsonLineageDispatcher {
 
     @Override
@@ -30,14 +29,24 @@ public class zxzhSparkline extends AbstractJsonLineageDispatcher {
             String replaceDate = StringUtils.replace(data, "ExecutionPlan (apiVersion: 1.2):", "");
             JSONObject operations = JSONObject.parseObject(replaceDate).getJSONObject("operations");
             JSONObject write = operations.getJSONObject("write");
-            JSONObject params = write.getJSONObject("params");
+            String type = write.getJSONObject("extra").getString("destinationType");
+            String downstreamUrn = "";
 
-            //目标数据库
-            String targetDatabase = params.getString("hoodie.datasource.hive_sync.database");
-            //目标表
-            String targetTablename = params.getString("hoodie.datasource.hive_sync.table");
+            if("jdbc".equals(type)){
+                String outputSource = write.getString("outputSource");
+                String dbType = outputSource.substring(outputSource.indexOf(":") + 1, outputSource.indexOf("://"));
+                String[] parts = StringUtils.substringAfterLast(outputSource, "/").split(":");
+                String targetGoal = parts[parts.length-1];
+                downstreamUrn= "{downstreamUrn: \\\"urn:li:dataset:(urn:li:dataPlatform:"+dbType+","+targetGoal+",PROD)\\\",";
+            }else if("hudi".equals(type)){
+                JSONObject params = write.getJSONObject("params");
+                //目标数据库
+                String targetDatabase = params.getString("hoodie.datasource.hive_sync.database");
+                //目标表
+                String targetTablename = params.getString("hoodie.datasource.hive_sync.table");
+                downstreamUrn= "{downstreamUrn: \\\"urn:li:dataset:(urn:li:dataPlatform:hive,"+targetDatabase+"."+targetTablename+",PROD)\\\",";
+            }
 
-            String json= "{downstreamUrn: \"urn:li:dataset:(urn:li:dataPlatform:hive,"+targetDatabase+"."+targetTablename+",PROD)\",";
             JSONArray readsArray = operations.getJSONArray("reads"); //这里开始获取数据来源
             Set<String> sourset =  new LinkedHashSet<String>();//定义一个不允许重复的集合
             for (int i = 0; i < readsArray.size(); i++) {
@@ -49,8 +58,8 @@ public class zxzhSparkline extends AbstractJsonLineageDispatcher {
 
             List jsonParamList = new ArrayList<String>();
             for(String s: sourset){
-                String upstreamUrn =   "upstreamUrn :  \"urn:li:dataset:(urn:li:dataPlatform:hive,"+s+",PROD)\"}";
-                String  jsonParam =   json+upstreamUrn;
+                String upstreamUrn =   "upstreamUrn :  \\\"urn:li:dataset:(urn:li:dataPlatform:hive,"+s+",PROD)\\\"}";
+                String  jsonParam =   downstreamUrn+upstreamUrn;
                 jsonParamList.add(jsonParam);
             }
 
@@ -62,10 +71,9 @@ public class zxzhSparkline extends AbstractJsonLineageDispatcher {
                 sb.append(jsonParamList.get(jsonParamList.size()-1));
             }
 
-            String spline = "{\"query\":\"mutation updateLineage { updateLineage( input:{ edgesToAdd : [" + sb + "],edgesToRemove: []})}\",\"variables\":{}}";
+            String spline = "{\"query\": \"mutation updateLineage { updateLineage( input:{ edgesToAdd : [" + sb + "],edgesToRemove: []})}\",\"variables\":{}}";
 
-          handleHttp(spline,"http://172.18.x.xx:9002/api/graphql");
-
+       handleHttp(spline,"http://172.18.x.xx:9002/api/graphql");
         }
 
         }
@@ -76,7 +84,7 @@ public class zxzhSparkline extends AbstractJsonLineageDispatcher {
             HttpPost request = new HttpPost(url);
 
             request.addHeader(HTTP.CONTENT_TYPE, "application/json");
-            request.addHeader("Authorization","Bearer xx");
+            request.addHeader("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhY3RvclR5cGUiOiJVU0VSIiwiYWN0b3JJZCI6ImRhdGFodWIiLCJ0eXBlIjoiUEVSU09OQUwiLCJ2ZXJzaW9uIjoiMiIsImp0aSI6IjYxMDVhMGVkLWNhZjctNDE5NC1hYmMzLTQ2NGU0ZDI5YTc1NSIsInN1YiI6ImRhdGFodWIiLCJpc3MiOiJkYXRhaHViLW1ldGFkYXRhLXNlcnZpY2UifQ.tFScljRefDvhOLM9pIQbjMIFw_HmkkYEcwlpFgNDLck");
 
             StringEntity s = new StringEntity(jsonParam, Charset.forName("UTF-8"));
             s.setContentEncoding("UTF-8");
