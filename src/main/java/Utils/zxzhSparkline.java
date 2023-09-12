78d296791b774c64679f66f52b7570f3c46ba5ab
javaht <java_ht@163.com>
2023-09-12 17:29:21 +0800

    fix

--- src/main/java/Utils/zxzhSparkline.java
+++ src/main/java/Utils/zxzhSparkline.java
@@ -3,7 +3,10 @@ package Utils;
 import Entitys.targetTable;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.linkedin.dataset.DatasetProperties;
+import com.linkedin.common.urn.DataPlatformUrn;
+import com.linkedin.common.urn.DatasetUrn;
+import com.linkedin.data.template.GetMode;
+import com.linkedin.schema.*;
 import datahub.client.Callback;
 import datahub.client.MetadataWriteResponse;
 import datahub.client.rest.RestEmitter;
@@ -19,6 +22,7 @@ import za.co.absa.spline.harvester.dispatcher.AbstractJsonLineageDispatcher;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.*;
 
@@ -35,21 +39,6 @@ public class zxzhSparkline extends AbstractJsonLineageDispatcher {
         if (data.startsWith("ExecutionPlan")) {
             String replaceDate = StringUtils.replace(data, "ExecutionPlan (apiVersion: 1.2):", "");
 
-             //这里存放的是目标表的类型和列名
-
-            List list = makeTargetTable(replaceDate);
-
-            list.stream().forEach( heihei->{
-                System.out.println(heihei);
-            });
-
-
-
-            //把表的元数据导入datahub
-
-
-
-
             //这里拼接的血缘
             StringBuilder sb = makeLine(replaceDate);
             //先删除原来的数据血缘
@@ -57,57 +46,129 @@ public class zxzhSparkline extends AbstractJsonLineageDispatcher {
             //增加最新的数据血缘
             String splineAdd = "{\"query\": \"mutation updateLineage { updateLineage( input:{ edgesToAdd : [" + sb + "],edgesToRemove: []})}\",\"variables\":{}}";
 
-            //handleHttp(splineRemove,"http://172.18.1.54:9002/api/graphql");
-           // handleHttp(splineAdd,"http://172.18.1.54:9002/api/graphql");
+           // handleHttp(splineRemove,"http://172.18.1.54:9002/api/graphql");
+         //   handleHttp(splineAdd,"http://172.18.1.54:9002/api/graphql");
 
         }
 
 
     }
 
-  public StringBuilder makeLine(String replaceDate){
-      String downstreamUrn = "";
-      JSONObject operations = JSONObject.parseObject(replaceDate).getJSONObject("operations");
-      JSONObject write = operations.getJSONObject("write");
-      String type = write.getJSONObject("extra").getString("destinationType");
+ /*
+ *
+ * 这个解析列级别的数据血缘
+ *
+ * */
+    public StringBuilder makeLine(String replaceDate){
+        String downstreamUrn = "";
+        JSONObject operations = JSONObject.parseObject(replaceDate).getJSONObject("operations");
+        JSONObject write = operations.getJSONObject("write");
+        String type = write.getJSONObject("extra").getString("destinationType");
 
-      if("hudi".equals(type)){
-          JSONObject params = write.getJSONObject("params");
-          //目标数据库
-          String targetDatabase = params.getString("hoodie.datasource.hive_sync.database");
-          //目标表
-          String targetTablename = params.getString("hoodie.datasource.hive_sync.table");
+        if("hudi".equals(type)){
+            JSONObject params = write.getJSONObject("params");
+            //目标数据库
+            String targetDatabase = params.getString("hoodie.datasource.hive_sync.database");
+            //目标表
+            String targetTablename = params.getString("hoodie.datasource.hive_sync.table");
 
-          downstreamUrn= "{downstreamUrn: \\\"urn:li:dataset:(urn:li:dataPlatform:hive,"+targetDatabase+"."+targetTablename+",PROD)\\\",";
-      }
+            downstreamUrn= "{downstreamUrn: \\\"urn:li:dataset:(urn:li:dataPlatform:hive,"+targetDatabase+"."+targetTablename+",PROD)\\\",";
+        }
 
-      JSONArray readsArray = operations.getJSONArray("reads"); //这里开始获取数据来源
-      Set<String> sourset =  new LinkedHashSet<String>();//定义一个不允许重复的集合
-      for (int i = 0; i < readsArray.size(); i++) {
-          JSONObject readObj = readsArray.getJSONObject(i);
-          String sourceTable = readObj.getJSONObject("params").getJSONObject("table").getJSONObject("identifier").getString("table");
-          String sourceDatabase = readObj.getJSONObject("params").getJSONObject("table").getJSONObject("identifier").getString("database");
-          sourset.add(sourceDatabase+"."+sourceTable);
-      }
+        JSONArray readsArray = operations.getJSONArray("reads"); //这里开始获取数据来源
+        Set<String> sourset =  new LinkedHashSet<String>();//定义一个不允许重复的集合
+        for (int i = 0; i < readsArray.size(); i++) {
+            JSONObject readObj = readsArray.getJSONObject(i);
+            String sourceTable = readObj.getJSONObject("params").getJSONObject("table").getJSONObject("identifier").getString("table");
+            String sourceDatabase = readObj.getJSONObject("params").getJSONObject("table").getJSONObject("identifier").getString("database");
+            sourset.add(sourceDatabase+"."+sourceTable);
+        }
 
-      List jsonParamList = new ArrayList<String>();
-      for(String s: sourset){
-          String upstreamUrn =   "upstreamUrn :  \\\"urn:li:dataset:(urn:li:dataPlatform:hive,"+s+",PROD)\\\"}";
-          String  jsonParam =   downstreamUrn+upstreamUrn;
-          jsonParamList.add(jsonParam);
-      }
+        List jsonParamList = new ArrayList<String>();
+        for(String s: sourset){
+            String upstreamUrn =   "upstreamUrn :  \\\"urn:li:dataset:(urn:li:dataPlatform:hive,"+s+",PROD)\\\"}";
+            String  jsonParam =   downstreamUrn+upstreamUrn;
+            jsonParamList.add(jsonParam);
+        }
 
-      StringBuilder sb = new StringBuilder();
-      for(int i = 0; i < jsonParamList.size()-1; i++) {
-          sb.append(jsonParamList.get(i)).append(",");
-      }
-      if(!jsonParamList.isEmpty()) {
-          sb.append(jsonParamList.get(jsonParamList.size()-1));
-      }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < jsonParamList.size()-1; i++) {
+            sb.append(jsonParamList.get(i)).append(",");
+        }
+        if(!jsonParamList.isEmpty()) {
+            sb.append(jsonParamList.get(jsonParamList.size()-1));
+        }
 
-      return sb;
+        return sb;
 
-  }
+    }
+
+
+
+    public  void  sendTargetTable(List<targetTable> list) throws IOException, URISyntaxException {
+
+       RestEmitter emitter = RestEmitter.create(b -> b.server("http://172.18.1x.54x:x").timeoutSec(100).token("x.x.x"));
+
+
+       SchemaMetadata metadata = new SchemaMetadata();
+       DatasetUrn.createFromString("urn:li:dataPlatform:hive");
+       String schemaname = "thisSchemaname";
+       String tablename = "tablename";
+       metadata.setSchemaName(schemaname);
+       SchemaMetadata.PlatformSchema  platformSchema = new       SchemaMetadata.PlatformSchema();
+       OtherSchema schema = new OtherSchema();
+       schema.setRawSchema("rawschema_hive");
+       platformSchema.setOtherSchema(schema);
+       metadata.setPlatformSchema(platformSchema);
+       DataPlatformUrn urn = new DataPlatformUrn("DataPlatformUrn_hive");
+       metadata.setPlatform(urn);
+       metadata.setVersion(0);
+       metadata.setHash("");
+       SchemaFieldArray fieldArray = new SchemaFieldArray();
+
+       for(targetTable table: list){
+           SchemaField field = new SchemaField();
+           field.setDescription("");
+           field.setFieldPath(table.getTablename());
+           field.setNativeDataType(table.getTabletype());
+           SchemaFieldDataType dataType =  field.getType(GetMode.valueOf(field.getNativeDataType()));
+           field.setType(dataType);
+           fieldArray.add(field);
+       }
+       metadata.setFields(fieldArray);
+
+       MetadataChangeProposalWrapper mcpw = MetadataChangeProposalWrapper.builder()
+               .entityType("dataset")
+               .entityUrn("urn:li:dataset:(urn:li:dataPlatform:hive,hudi_dwd.test,PROD)")
+               .upsert()
+               .aspect(metadata)
+               .build();
+
+       emitter.emit(mcpw, new Callback() {
+           @Override
+           public void onCompletion(MetadataWriteResponse response) {
+               if (response.isSuccess()) {
+                   System.out.println(String.format("Successfully emitted metadata event for %s", mcpw.getEntityUrn()));
+               } else {
+                   // Get the underlying http response
+                   HttpResponse httpResponse = (HttpResponse) response.getUnderlyingResponse();
+                   System.out.println(String.format("Failed to emit metadata event for %s, aspect: %s with status code: %d", mcpw.getEntityUrn(), mcpw.getAspectName(), httpResponse.getStatusLine().getStatusCode()));
+
+
+               }
+           }
+
+           @Override
+           public void onFailure(Throwable exception) {
+               System.out.println(
+                       String.format("Failed to emit metadata event for %s, aspect: %s due to %s", mcpw.getEntityUrn(),
+                               mcpw.getAspectName(), exception.getMessage()));
+           }
+       });
+
+
+
+   }
 
 
     /*
